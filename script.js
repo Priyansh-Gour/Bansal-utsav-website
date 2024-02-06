aba0838d90ea4feb0afba42eb1696b7eae8d5aa1
Priyansh-Gour <priyanshgour817@gmail.com>
2024-02-06 06:21:32 +0530

    added navbar animations

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 var countDownDate = new Date("February 22, 2024 23:59:59").getTime();
 
-      var x = setInterval(function () {
+var x = setInterval(function () {
+  var now = new Date().getTime();
 
-        var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-    
-        var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("dday").innerHTML = days;
+  document.getElementById("dhour").innerHTML = hours;
+  document.getElementById("dmin").innerHTML = minutes;
+  document.getElementById("dsec").innerHTML = seconds;
 
-        document.getElementById("dday").innerHTML = days;
-        document.getElementById("dhour").innerHTML = hours;
-        document.getElementById("dmin").innerHTML = minutes;
-        document.getElementById("dsec").innerHTML = seconds;
+  if (distance < 0) {
+    clearInterval(x);
+  }
+}, 1000);
 
-    
-        if (distance < 0) {
-          clearInterval(x);
-        }
-      }, 1000);
+var tl = gsap.timeline();
+
+
+// gsap animation
+tl.from('.navbar', {
+    stagger: .3,
+    duration: 1,
+    y: 20,
+    delay: 1,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+})
+.from('.heading', {
+    stagger: .3,
+    duration: 1,
+    y: 20,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+}, '-=2')
